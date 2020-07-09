0ea4ae1953e540192050712758f6f658b78a9170
Bradley Espinoza <bespinoza@doximity.com>
2020-07-08 17:13:35 -0700

    Updated splash image

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,39 @@
 class RecipesController < ApplicationController
-end
 
-def show
+  def show
+    @recipe = Recipe.find_by(params[:id])
+  end
 
-end
+  def index
+    @recipes = Recipe.all
+  end
 
-def index
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+        flash[:notice] = "Article was created successfully"
+        redirect_to @recipe
+      else
+        render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe)
+  end
 
 end
